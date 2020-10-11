93c62b87093521a090e4d3dac24bada0caf601f4
Alejandro Gonzalez <alexgve7sv@outlook.com>
2020-10-11 09:36:58 -0600

    adding cover to app

--- lib/pages/landing.dart
+++ lib/pages/landing.dart
@@ -1,10 +1,22 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 class LandingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage('assets/cover.jpg')
+          )
+        ),
+        child: Container()
+      )
     );
   }
+
+  
 }
