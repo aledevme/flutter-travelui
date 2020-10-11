be625bfb3397965a79317bf9c978b4923c54e6b2
Alejandro Gonzalez <alexgve7sv@outlook.com>
2020-10-10 20:49:05 -0600

    adding landing screen 📱

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_mountain/pages/landing.dart';
  
 void main() => runApp(MyApp());
  
@@ -7,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      routes: {
+        '/' : (BuildContext context) => LandingScreen()
+      },
     );
   }
 }
