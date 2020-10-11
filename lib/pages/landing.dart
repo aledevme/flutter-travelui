895e0c4931304ad9017eb6a2f351c575ba977301
Alejandro Gonzalez <alexgve7sv@outlook.com>
2020-10-11 09:41:16 -0600

    adding options to blur box

--- lib/pages/landing.dart
+++ lib/pages/landing.dart
@@ -35,7 +35,60 @@ class LandingScreen extends StatelessWidget {
                 ),
               )
             ),
-            
+            Positioned(
+              child: ClipRRect(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: 0.1,
+                    sigmaY: 0.1
+                  ),
+                  child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: 320,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.1),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20)
+                    )
+                  ),
+                  padding: EdgeInsets.all(30),
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Column(
+                            children: [
+                              Text('Places', style: TextStyle(
+                              fontSize: 20,
+                                color: Colors.white
+                              )),
+                              Container(color: Colors.black,height: 2, width: 60)
+                            ],
+                          ),
+                          SizedBox(width: 20),
+                          Text('Hotels', style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white
+                          )),
+                          SizedBox(width: 20),
+                            Text('Restaurants', style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white
+                          )),
+                        ],
+                      ),
+                      
+                    ],
+                  )
+                ),
+              ),
+                ),
+              )
+            )
           ],
         )
       )
