cf74a664fe2ad4fef15959235591db587f04e218
Alejandro Gonzalez <alexgve7sv@outlook.com>
2020-10-12 01:50:31 -0600

    header app - home screen fixed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,17 +5,28 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
       body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 80
-          ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              _headerApp()
+             Container(
+               padding: EdgeInsets.only(
+                 top: 70,
+                 left: 20,
+                 right: 20,
+                 bottom: 20
+               ),
+               color: Colors.grey[200],
+               child: Column(
+                 children: [
+                    _headerApp(),
+                    _options(),
+                    SizedBox(height: 20),
+                    _searchField()
+                 ],
+               ),
+             )
             ],
           ),
         ),
@@ -30,38 +41,84 @@ class Home extends StatelessWidget {
       children: [
         Container(
           child: Text('Discover', style: TextStyle(
-            fontSize: 30
+            fontSize: 40,
+            fontWeight: FontWeight.w500,
           )),
         ),
         Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: Image(
-                  height: 60,
-                  width: 60,
-                  image: AssetImage('assets/profile.png'),
-                ),
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Image(
+                height: 50,
+                width: 50,
+                image: AssetImage('assets/profile.png'),
               ),
-              Positioned(
-                left: 43,
-                bottom: 43,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.red,
-                  ),
-                  width: 17,
-                  height: 17,
-                  child: Text('2', style: TextStyle(
-                    color: Colors.white
-                  )),
-                  alignment: Alignment.center,
+            ),
+            Positioned(
+              left: 33,
+              bottom: 33,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.red,
                 ),
-              )
-            ],
-          ),
+                width: 17,
+                height: 17,
+                child: Text('2', style: TextStyle(
+                  color: Colors.white, 
+                )),
+                alignment: Alignment.center,
+              ),
+            )
+          ],
+        ),
       ],
+    );
+  }
+
+  Widget _options(){
+    return Row(
+      children: [
+        Column(
+          children: [
+            Text('Places', style: TextStyle(
+            fontSize: 20,
+              color: Colors.black
+            )),
+            SizedBox(height: 5),
+            Container(color: Colors.black,height: 2, width: 60)
+          ],
+        ),
+        SizedBox(width: 20),
+        Text('Hotels', style: TextStyle(
+          fontSize: 20,
+          color: Colors.black
+        )),
+        SizedBox(width: 20),
+          Text('Restaurants', style: TextStyle(
+          fontSize: 20,
+          color: Colors.black
+        )),
+      ],
+    );
+  }
+
+  Widget _searchField(){
+    return TextField(
+      decoration: InputDecoration(
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.white),
+          borderRadius: BorderRadius.circular(5)
+        ),
+        filled: true,
+        fillColor: Colors.white,
+        prefixIcon: Icon(Icons.search, color: Colors.grey),
+        hintText: 'Search for your dream trip...',
+        focusedBorder:OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.white, width: 2.0),
+        ),
+      ),
     );
   }
 }
